a90916c86d9091b834362dd5f43be1d78e96aaf4
Pete Cridland <peter.cridland@kpmg.co.uk>
2024-04-11 09:52:11 +0100

    Test database connection

--- src/template/app.py
+++ src/template/app.py
@@ -1,14 +1,32 @@
 import psycopg2
 from os import environ
 from boto3 import client
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(environ.get("LOG_LEVEL", "DEBUG"))
+
 
 def lambda_handler(event, context):
+    logger.debug('Getting password from secrets manager')
     secrets_manager = client('secretsmanager')
     response = secrets_manager.get_secret_value(
         SecretId=environ.get('POSTGRES_PASSWORD'),
 
     )
     pg_pass = response['SecretString']
-    if len(pg_pass) > 0:
-        print('got it')
-    print("Hello World")
+    logger.debug('Got password')
+
+    logger.debug('Connecting to DB')
+    pg_host = environ.get('POSTGRES_HOST')
+    pg_db = environ.get('POSTGRES_DB')
+    pg_user = environ.get('POSTGRES_USER')
+    postgres_connection = psycopg2.connect(f"dbname='{pg_db}' user='{pg_user}' host='{pg_host}' password='{pg_pass}'")
+    logger.debug('Connected to DB')
+
+    logger.debug('Getting sample data')
+    with postgres_connection.cursor() as curs:
+        curs.execute('select * from data_quality_dataqualityreport limit 1')
+        logger.debug(curs.fetchone())
+    logger.debug('Got sample data')
+
+    postgres_connection.close()
