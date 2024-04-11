5e52b40b498d92f7ba3bb603922331e5240bfb36
Pete Cridland <peter.cridland@kpmg.co.uk>
2024-04-11 09:06:18 +0100

    checking secret manager integration

--- src/template/app.py
+++ src/template/app.py
@@ -1,2 +1,14 @@
+import psycopg2
+from os import environ
+from boto3 import client
+
 def lambda_handler(event, context):
+    secrets_manager = client('secretsmanager')
+    response = client.get_secret_value(
+        SecretId=environ.get('POSTGRES_PASSWORD'),
+
+    )
+    pg_pass = response['SecretString']
+    if len(pg_pass) > 0:
+        print('got it')
     print("Hello World")
