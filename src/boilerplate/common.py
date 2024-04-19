50f364233f59884b26e8313a83a09802c2ed595a
Pete Cridland <peter.cridland@kpmg.co.uk>
2024-04-19 08:46:15 +0100

    Test layered database

--- src/boilerplate/common.py
+++ src/boilerplate/common.py
@@ -1,2 +1,31 @@
-def test():
-    return "Test layer"
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+import geoalchemy2
+from os import environ
+from boto3 import client
+import logging
+from common import test
+logger = logging.getLogger(__name__)
+logger.setLevel(environ.get("LOG_LEVEL", "DEBUG"))
+
+class BodsDB:
+    def __init__(self):
+        logger.debug('Getting DB password from secrets manager')
+        secrets_manager = client('secretsmanager')
+        password_response = secrets_manager.get_secret_value(
+            SecretId=environ.get('POSTGRES_PASSWORD'),
+        )
+        pg_pass = password_response['SecretString']
+        logger.debug('Got DB password')
+
+        logger.debug('Connecting to DB')
+        pg_host = environ.get('POSTGRES_HOST')
+        pg_db = environ.get('POSTGRES_DB')
+        pg_user = environ.get('POSTGRES_USER')
+        pg_port = environ.get('POSTGRES_PORT')
+        self.sqlalchemy_base = automap_base()
+        sqlalchemy_engine = create_engine(f"postgresql+psycopg2://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
+        self.sqlalchemy_base.prepare(autoload_with=sqlalchemy_engine)
+        self.session = Session(sqlalchemy_engine)
+        logger.debug('Connected to DB')
--- src/template/app.py
+++ src/template/app.py
@@ -2,33 +2,13 @@ import psycopg2
 from os import environ
 from boto3 import client
 import logging
-from common import test
+from common import BodsDB
 logger = logging.getLogger(__name__)
 logger.setLevel(environ.get("LOG_LEVEL", "DEBUG"))
 
 
 def lambda_handler(event, context):
-    logger.debug('Getting password from secrets manager')
-    secrets_manager = client('secretsmanager')
-    response = secrets_manager.get_secret_value(
-        SecretId=environ.get('POSTGRES_PASSWORD'),
-
-    )
-    pg_pass = response['SecretString']
-    logger.debug('Got password')
-
-    logger.debug('Connecting to DB')
-    pg_host = environ.get('POSTGRES_HOST')
-    pg_db = environ.get('POSTGRES_DB')
-    pg_user = environ.get('POSTGRES_USER')
-    postgres_connection = psycopg2.connect(f"dbname='{pg_db}' user='{pg_user}' host='{pg_host}' password='{pg_pass}'")
-    logger.debug('Connected to DB')
-
-    logger.debug('Getting sample data')
-    with postgres_connection.cursor() as curs:
-        curs.execute('select * from data_quality_dataqualityreport limit 1')
-        logger.debug(curs.fetchone())
-    logger.debug('Got sample data')
-
-    postgres_connection.close()
-    logger.debug(f"Layer returned {test()}")
+    db = BodsDB()
+    archive = BodsDB.sqlalchemy_base.classes.avl_cavldataarchive
+    first_archive_record = db.session.query(archive).first()
+    logger.debug(first_archive_record.__dict__)
