3d43b0524d1d004d32d01607154ed8e95d6e5f91
Nitish Arora <nitisharora@kpmg.com>
2024-08-28 17:37:19 +0530

    Initial commit

---
+++ src/boilerplate/otc_inactiveservice.py
@@ -0,0 +1,46 @@
+from common import Check
+from dqs_logger import logger
+from sqlalchemy import and_
+from datetime import datetime
+
+
+class OtcInactiveService:
+    """
+    Class to handle the otc_inactiveservice table in database.
+
+    Methods:
+    is_service_exists: Is the service exists in otc_inactiveservice table for service with gte today's date
+    """
+
+    def __init__(self, check: Check):
+        self._check = check
+        self._table = check.db.classes.otc_inactiveservice
+
+    def is_service_exists(self, registration_number: str):
+        """
+        Method to check whether the service exists in otc_inactiveservice
+        table for service with greater than today's date
+        """
+        curr_date = datetime.now().date()
+        try:
+            result = (
+                self._check.db.session.query(self._table.registration_number)
+                .where(
+                    (self._table.registration_number == registration_number)
+                    & (self._table.effective_date >= curr_date)
+                )
+                .first()
+            )
+
+            if result:
+                return True
+            return False
+
+        except Exception as e:
+            logger.error(
+                (
+                    f"Attempting to fetch details of otc_inactiveservice for registration number = {str(registration_number)} and effective date > {curr_date}"
+                ),
+                e,
+            )
+            raise e
---
+++ src/boilerplate/otc_service.py
@@ -0,0 +1,37 @@
+from common import Check
+from dqs_logger import logger
+
+
+class OtcService:
+    """
+    Class to handle the otc_service table in database.
+
+    Methods:
+    is_service_exists: Is the service exists in otc_service table with filter on registration number
+    """
+
+    def __init__(self, check: Check):
+        self._check = check
+        self._table = check.db.classes.otc_service
+
+    def is_service_exists(self, registration_number: str):
+        """
+        Method to get the details by registration number
+        """
+        try:
+            result = (
+                self._check.db.session.query(self._table.registration_number)
+                .where(self._table.registration_number == registration_number)
+                .first()
+            )
+
+            if result:
+                return True
+            return False
+
+        except Exception as e:
+            logger.error(
+                f"Attempting to fetch details of otc_service for registration number = {str(registration_number)}",
+                e,
+            )
+            raise e
