09c2f19f05178a666b909fc0a138f07ca7bef097
Nitish Arora <nitisharora@kpmg.com>
2024-10-22 17:40:15 +0530

    Fixing the timeout

--- src/template/serviced_organisation_out_of_date.py
+++ src/template/serviced_organisation_out_of_date.py
@@ -6,16 +6,14 @@ from time_out_handler import TimeOutHandler
 from dqs_exception import LambdaTimeOutError
 from dataframes import get_df_serviced_organisation
 from datetime import datetime
+from time_out_handler import TimeOutHandler, get_timeout
 
 
-def lambda_handler(event, context):
+def lambda_worker(event, check):
     status = DQSTaskResultStatus.SUCCESS.value
 
     try:
-        check = Check(event)
         observation = ObservationResult(check)
-        check.validate_requested_check()
-
         df = get_df_serviced_organisation(check)
         if not df.empty:
 
@@ -63,3 +61,22 @@ def lambda_handler(event, context):
     finally:
         check.set_status(status)
     return
+
+
+def lambda_handler(event, context):
+    try:
+        # Get timeout from context reduced by 15 sec
+        timeout = get_timeout(context)
+        check = Check(event)
+        check.validate_requested_check()
+        timeout_handler = TimeOutHandler(event, check, timeout)
+        timeout_handler.run(lambda_worker)
+
+    except LambdaTimeOutError:
+        status = DQSTaskResultStatus.TIMEOUT.value
+        logger.info(f"Set status to {status}")
+        check.set_status(status)
+    except Exception as e:
+        status = DQSTaskResultStatus.FAILED.value
+        logger.error(f"Check status failed due to {e}")
+        check.set_status(status)
