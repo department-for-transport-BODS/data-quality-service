7f758a834580bb0636366082f46fd226a772c705
SamerIssam <samer.salem@kpmg.co.uk>
2025-01-30 16:38:43 +0000

    add map

---
+++ src/template/lambda-invoke.py
@@ -0,0 +1,7 @@
+from common import Check
+
+def lambda_handler(event, context):
+    # check = Check(event, "")
+    data = [{'file_id':'39070'},{'file_id':'39070'},{'file_id':'39070'}]
+    return data
+
