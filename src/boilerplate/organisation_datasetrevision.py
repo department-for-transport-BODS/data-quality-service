282a8be2e2a15f1bf248caedc5d3f2e7d0ff60c7
SamerIssam <samer.salem@kpmg.co.uk>
2025-01-31 17:10:29 +0000

    remove all func

--- src/boilerplate/organisation_datasetrevision.py
+++ src/boilerplate/organisation_datasetrevision.py
@@ -9,10 +9,9 @@ class Revision:
 
     def get_revision(self,revision_id) -> pd.DataFrame:
         try:
-            query = self._db.session.query(self._table_name).filter(
+            result = self._db.session.query(self._table_name).filter(
                 self._table_name.id == revision_id,
             ).first()
-            result = query.all()
             logger.info(f"Rfevision results: {result}")
             return result
         except Exception as e:
