544880dcdd6099d72effd4151ab800dc57f164c1
SamerIssam <samer.salem@kpmg.co.uk>
2025-01-31 18:36:28 +0000

    update task results

--- src/boilerplate/dqs_task_results.py
+++ src/boilerplate/dqs_task_results.py
@@ -21,24 +21,25 @@ class DQTaskResults:
             raise e
         return txc_file_attributes_list
 
-    def initialize_task_results(self, report_id: int, combinations: object) -> object:
-        """
-        Create a TaskResults object based on the given revision, TXCFileAttribute,
-        and Check objects.
-        """
-        task_results_to_create = []
-        for txc_file_attribute_id, check_id in combinations:
-            logger.info(f"the comps list: {txc_file_attribute_id}: {check_id}")   
-            task_results_to_create.append({
-                "status": TaskResultsStatus.PENDING.value,
-                "message": "",
-                "checks_id": check_id,
-                "dataquality_report_id": report_id,
-                "transmodel_txcfileattributes_id": txc_file_attribute_id,
-            })
+def initialize_task_results(self, report_id: int, combinations: object) -> object:
+    """
+    Create a TaskResults object based on the given revision, TXCFileAttribute,
+    and Check objects.
+    """
+    task_results_to_create = []
+    for txc_file_attribute_id, check_id in combinations:
+        logger.info(f"the comps list: {txc_file_attribute_id}: {check_id}")   
+        task_results_to_create.append({
+            "status": TaskResultsStatus.PENDING.value,
+            "message": "",
+            "checks_id": check_id,
+            "dataquality_report_id": report_id,
+            "transmodel_txcfileattributes_id": txc_file_attribute_id,
+        })
 
-        self._db.session.execute(
-            insert("dqs_taskresults"),  # Table name as a string
-            task_results_to_create
-        )
-        self._db.session.commit()
+    # Execute the insert
+    self._db.session.execute(
+        insert(self._table_name),  
+        task_results_to_create
+    )
+    self._db.session.commit()
