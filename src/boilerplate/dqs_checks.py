2160fb1ceefcaf587be4867c613715b99d011561
SamerIssam <samer.salem@kpmg.co.uk>
2025-01-31 18:04:54 +0000

    edit query

--- src/boilerplate/dqs_checks.py
+++ src/boilerplate/dqs_checks.py
@@ -1,25 +1,18 @@
-from sqlalchemy import update
 import pandas as pd
 from dqs_logger import logger
 from common import BodsDB
-from contextlib import contextmanager
-from enums import ReportStatus, TaskResultsStatus
-
+from typing import List
 
 class DQChecks:
     def __init__(self):
         self._db = BodsDB()
         self._table_name = self._db.classes.dqs_checks
 
-    def get_all_checks(self) -> pd.DataFrame:
-        df = pd.DataFrame()
+    def get_all_checks(self) -> List:
         try:
-            query = self._db.session.query(self._table_name).all()
-            # list of all the checks
-            checks_list = [check.to_dict() for check in query]
-            df = pd.read_sql_query(query.statement, self._db.session.bind)
+            result = self._db.session.query(self._table_name).all()
         except Exception as e:
-            logger.error(f"Failed to add observation for check = pipeline_monitor: {e}")
+            logger.error(f"Field to retrieve checks : {e}")
             raise e
-        return checks_list
+        return result
 
--- src/boilerplate/org_txcfileattributes.py
+++ src/boilerplate/org_txcfileattributes.py
@@ -1,25 +1,22 @@
-import pandas as pd
 from dqs_logger import logger
 from common import BodsDB
-
+from typing import List
 class TXCFileAttributes:
     def __init__(self):
         self._db = BodsDB()
         self._table_name = self._db.classes.organisation_txcfileattributes
         self.service = self._db.classes.transmodel_service
 
-    def get_all_txc_file_attributes(self,revision_id) -> pd.DataFrame:
-        df = pd.DataFrame()
+    def get_all_txc_file_attributes(self,revision_id) -> List:
         try:
-            query = self._db.session.query(self.service.txcfileattributes_id).filter(
+            result = self._db.session.query(self.service.txcfileattributes_id).filter(
                 self.service.revision_id == revision_id,
-            ).union()
-            txc_file_attributes_list = [txc_file_attributes.to_dict() for txc_file_attributes in query]
-            # df = pd.read_sql_query(query.statement, self._db.session.bind)
+            ).distinct()
+            logger.info(f"txc_file query result: {result}")
         except Exception as e:
-            logger.error(f"Failed to add observation for check = pipeline_monitor: {e}")
+            logger.error(f"Failed to retrieve txc_file ids: {e}")
             raise e
-        return txc_file_attributes_list
+        return result
 
 
 
