ad79136e672fbe35fbd572cf3f88595f3e54bdca
SamerIssam <samer.salem@kpmg.co.uk>
2025-01-31 17:02:49 +0000

    fix classes

--- src/template/initiate_lambda.py
+++ src/template/initiate_lambda.py
@@ -9,13 +9,13 @@ from dqs_task_results import DQTaskResults
 def lambda_handler(event, context):
     revision_id = event.get('revision_id','')
     if revision_id:
-        revision = Revision.get_revision(revision_id)
-        report = DQReport.initialise_dqs_task(revision)
+        revision = Revision().get_revision(revision_id)
+        report = DQReport().initialise_dqs_task(revision)
         logger.info(f"report ${report}")
-        checks = DQChecks.get_all_checks()
-        txc_files = TXCFileAttributes.get_all_txc_file_attributes(revision_id=revision_id)
+        checks = DQChecks().get_all_checks()
+        txc_files = TXCFileAttributes().get_all_txc_file_attributes(revision_id=revision_id)
         combinations = itertools.product(txc_files, checks)
-        DQTaskResults.initialize_task_results(report, combinations)
+        DQTaskResults().initialize_task_results(report, combinations)
     else:
         logger.error(f"No revision id in even: {event}")
 
