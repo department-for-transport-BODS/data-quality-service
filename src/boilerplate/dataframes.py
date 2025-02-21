f2a9aa62b5d663214f8e27b0cbd9adf8f6c073ba
SamerIssam <samer.salem@kpmg.co.uk>
2025-02-21 10:48:32 +0000

    update df file

--- src/boilerplate/dataframes.py
+++ src/boilerplate/dataframes.py
@@ -523,35 +523,3 @@ def get_naptan_availablilty(check: Check, atco_codes: set[String]) -> pd.DataFra
     df = pd.read_sql_query(result.statement, check.db.session.bind)
     df["atco_code_exists"] = df["atco_code"].apply(lambda cell: cell in atco_codes)
     return df
-
-
-
-@contextmanager
-def update_taskresult_status(report, file_id,obervation, status):
-    """
-    Get the dataframe containing the vehicle journey and the stop activity
-
-    """
-    dqs_taskresults = report.db.classes.dqs_taskresults
-    dqs_report = report.db.classes.dqs_report
-    dqs_checks = report.db.classes.dqs_checks
-    try:
-        if status:
-            update_task_results = (
-                dqs_taskresults.filter(
-                    dqs_taskresults.dataquality_report_id.in_(report.ids)
-                )
-                .filter(dqs_checks.id.in_(dqs_checks.observation == obervation))
-                .filter(dqs_report.id == report.report_id)
-                .filter(dqs_taskresults.transmodel_txcfileattributes_id == file_id)
-            )
-        
-            for record in update_task_results:
-                record.status = status
-            report.db.session.commit()
-    except Exception as e:
-        logger.error(
-            f"Failed to update task result status for check = pipeline_monitor: {e}"
-        )
-        report.db.session.rollback()
-        raise e
