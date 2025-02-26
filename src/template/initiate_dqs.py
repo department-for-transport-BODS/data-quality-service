294232d9bc7525372f09291ec4fa1213b936a3cc
SamerIssam <samer.salem@kpmg.co.uk>
2025-02-26 13:45:53 +0000

    initial commit

--- src/template/initiate_dqs.py
+++ src/template/initiate_dqs.py
@@ -1,9 +1,12 @@
+import sys
+sys.path.append("../boilerplate")
 import itertools
 from dqs_checks import DQChecks
 from dqs_report import DQReport
 from dqs_logger import logger
 from org_txcfileattributes import TXCFileAttributes
 from dqs_task_results import DQTaskResults
+
 
 def get_report_id(revision_id: int) -> int:
     """
@@ -26,13 +29,16 @@ def get_txc_files(revision_id: int) -> list:
     txc_files=TXCFileAttributes().get_all_txc_file_attributes(revision_id=revision_id)
     return txc_files
 
-def lambda_handler(event, context):
-    revision_id = event.get('DatasetRevisionId','')
+def lambda_handler(event=None, context=None):
+    # revision_id = event.get('DatasetRevisionId','')
+    
+    revision_id = 201
     try:
         if revision_id:
             report_id = get_report_id(revision_id)
             check_ids = get_check_ids_list()
             txc_files = get_txc_files(revision_id)
+            print(report_id, check_ids, txc_files)
             combinations = itertools.product(txc_files, check_ids)
             DQTaskResults().initialize_task_results(report_id, combinations)
             dict_txc_files = list(map(lambda x: {"file_id": x}, txc_files))
@@ -45,3 +51,6 @@ def lambda_handler(event, context):
         logger.exception(e)
         
 
+
+if __name__ == "__main__":
+    lambda_handler()
